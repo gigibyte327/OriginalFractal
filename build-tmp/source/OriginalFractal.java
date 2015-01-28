d7a3c419e7fd017ae70f3ae1e762d1cd0eb3d107
unknown <gilu@697-334-Edoras.sf.k12.internal>
2015-01-28 10:42:28 -0800

    Finished circly

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -1,14 +1,30 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
 // private double fractionLength = .8; 
-private double fractR = .8;
+private double fractR = .8f;
 // private int smallestBranch = 10; 
 private int smallestCirc=5;
 // private double branchAngle = .2;  
-private double circAng=.3;
+private double circAng=.3f;
 
 public void setup() 
 {   
 	size(640,480);    
-	noLoop(); 
+	//noLoop(); 
 } 
 public void draw() 
 {   
@@ -16,7 +32,7 @@ public void draw()
 // 	stroke(0,255,0);   
 // 	line(320,480,320,380);   
 // 	drawBranches(320,380,10,3*(Math.PI/2)); 
-    drawElf(320,240,480,mouseX);
+    drawElf(mouseX,240,480,mouseX);
     fill(255);
     line(320,0,320,480);
 } 
@@ -48,3 +64,12 @@ public void drawElf(int x, int y, float circR, int blurb){
 //     	drawBranches(endX2,endY2,branchLength-1,angle-Math.PI/4);
 //     }
 // }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
